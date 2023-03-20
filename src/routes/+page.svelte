e7d09ef4006d8e779108214375db150d59df4e14
Wild-Queue <eshalagin@list.ru>
2023-03-20 21:34:38 +0300

    Homework5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,52 +6,52 @@
 	<meta name="description" content="Shalagin Egor portfolio on github page" />
 </svelte:head>
 
-<div>
+<div style="width: 900px; margin: auto;">
 	<h1>Hello everyone, my name is Shalagin Egor and everyone you see on this page is me</h1>
 	<img
 		title="ShalaginEgor6"
 		alt="ShalaginEgor6"
-		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/face.png"
+		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor6.jpg"
 		width="225"
-		style="float:left; margin: 0 10px 0 10px; transform: rotate(15deg);"
+		style="float:left; margin: 0 10px 0 70px; transform: rotate(15deg);"
 	/>
 	<img
 		title="ShalaginEgor5"
 		alt="ShalaginEgor5"
-		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/face.png"
+		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor5.jpg"
 		width="250"
-		style="float:left; margin: 50px 0 0 50px; rotate(15deg);"
+		style="float:left; margin: 50px 0 0 50px; transform: rotate(-8deg);"
 	/>
 
 	<img
 		title="ShalaginEgor4"
 		alt="ShalaginEgor4"
-		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/face.png"
+		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor4.jpg"
 		width="220"
-		style="float:left; margin: 0 0 0 50px; transform: rotate(7deg);"
+		style="float:left; margin: 50px 0 0 50px; transform: rotate(15deg);"
 	/>
 
 	<img
 		title="ShalaginEgor3"
 		alt="ShalaginEgor3"
-		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/face.png"
+		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor3.jpg"
 		width="220"
-		style="float:left; margin: 200px 20px 0 10px;"
+		style="float:right; margin: 80px 20px 0 20px; transform: rotate(8deg);"
 	/>
 
 	<img
 		title="ShalaginEgor2"
 		alt="ShalaginEgor2"
-		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/face.png"
+		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor2.jpg"
 		width="220"
-		style="float:left; margin: 200px 20px 0 10px;"
+		style="float:right; margin: 80px 20px 0 10px;"
 	/>
 	<img
 		title="ShalaginEgor1"
 		alt="ShalaginEgor1"
 		src="https://raw.githubusercontent.com/Wild-Queue/portfolio/main/pictures/ShalaginEgor1.jpg"
-		width="200"
-		style="float:left; margin: 0 10px 0 10px;"
+		width="250"
+		style="float:right; margin: 40px 50px 0 10px; transform: rotate(-15deg);"
 	/>
 </div>
 
---
+++ src/routes/portfolio/About.svelte
@@ -0,0 +1,36 @@
+<div style="font-size: 125%;">
+    Software development student in Innopolis. 2-year experience in back-end
+    development with C++, C# and Python and front-end development with
+    JavaScript.
+    <br />Flexible, stress resistent, good at working under time pressure. I am
+    passionate about developing and maintaining complex software structures and
+    providing a user-friendly interface.
+    <br />English Upper-Intermediate.
+    <br />
+    <div style="font-size: 90%; margin: 15px 0 0 25%">
+        <a id="tg-link" href="https://t.me/Shalagin_Egor">
+            <i class="fa fa-telegram"/>@Shalagin_Egor
+        </a>
+        <a id="email" href="mailto:shalagin.e.s@gmail.com">
+            <i class="fa fa-envelope" style="color:black;"/>shalagin.e.s@gmail.com
+        </a>
+        <a id="li-link" href="https://www.linkedin.com/in/shalagin-egor/">
+            <i class="fa fa-linkedin"/>shalagin-egor
+        </a>
+        <a href="https://github.com/Wild-Queue/">
+            <i class="fa fa-github" style="color:black;"/>Wild-Queue
+        </a>
+        <br />
+    </div>
+</div>
+
+<style>
+    a {
+        color: rgb(30, 30, 211);
+        margin: 0 1% 0 0;
+    }
+    i {
+        color: rgb(30, 30, 211);
+        margin-right: 0.5%;
+    }
+</style>
